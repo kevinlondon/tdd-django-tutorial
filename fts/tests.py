e12e7de02f32a2169a737be3adf079b7919e6567
Kaedon <kevinlondon@gmail.com>
2012-12-20 00:06:17 -0800

    End of part 2. Add Choices to Poll and more to tests.

--- fts/tests.py
+++ fts/tests.py
@@ -36,6 +36,47 @@ class PollsTest(LiveServerTestCase):
         polls_links = self.browser.find_elements_by_link_text('Polls')
         self.assertEquals(len(polls_links), 2)
 
-        # Todo: Use the admin site to create a new Poll
-        self.fail('todo: finish tests')
+        # Click second poll link
+        polls_links[1].click()
+
+        # She is taken to the polls listing page, which shows she has
+        # no polls yet
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('0 polls', body.text)
+
+        # Add a new poll.
+        new_poll_link = self.browser.find_element_by_link_text('Add poll')
+        new_poll_link.click()
+
+        # See input fields for Question and Date Published
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Question:', body.text)
+        self.assertIn('Date published:', body.text)
         
+        # Add a question for the poll
+        question_field = self.browser.find_element_by_name('question')
+        question_field.send_keys('How awesome is Test-Driven Development?')
+
+        # Set time and date of publication.
+        date_field = self.browser.find_element_by_name('pub_date_0')
+        date_field.send_keys('01/01/12')
+        time_field = self.browser.find_element_by_name('pub_date_1')
+        time_field.send_keys('00:00')
+
+        # Enter additional choices for the poll.
+        choice_1 = self.browser.find_element_by_name('choice_set-0-choice')
+        choice_1.send_keys('Very awesome')
+        choice_2 = self.browser.find_element_by_name('choice_set-1-choice')
+        choice_2.send_keys('Quite awesome')
+        choice_3 = self.browser.find_element_by_name('choice_set-2-choice')
+        choice_3.send_keys('Moderately awesome')
+
+        # Selects save.
+        save_button = self.browser.find_element_by_css_selector("input[value='Save']")
+        save_button.click()
+
+        # Return to polls listing. See new poll.
+        new_poll_links = self.browser.find_elements_by_link_text(
+                "How awesome is Test-Driven Development?"
+        )
+        self.assertEquals(len(new_poll_links), 1)
