fee61633b886f478d57c897e1f6e70474822bccb
Kevin London <kevinlondon@gmail.com>
2013-03-12 22:53:39 -0700

    Up to 'Writing the FT as comments'

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-
+    url(r'^$', 'polls.views.home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from polls.models import Poll
+
+def home(request):
+    content = ''
+    for poll in Poll.objects.all():
+        content += poll.question
+
+    return HttpResponse(content)
